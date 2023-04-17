eec8dbc66fec2bf1a74a2eec69ca07ed450379fa
estsouza <estsouza@gmail.com>
2023-04-17 07:39:42 -0300

    query geo demographic data

--- demographic_data_access.R
+++ demographic_data_access.R
@@ -1,22 +1,36 @@
 # install.packages("tidycensus")
 require(tidycensus)
+source("config.R")
+census_api_key(census.gov_api_key)
 
 states <- tidycensus::fips_codes %>%
-  dplyr::select(state_name) %>%
+  dplyr::select(state_name, state) %>%
   unique() |>
-  pull(state_name)
+  pull(state)
 
-selected_state <- "California"
+selected_state <- "CA"
 counties <- tidycensus::fips_codes %>%
-    dplyr::filter(state_name == selected_state) %>%
+    dplyr::filter(state == selected_state) %>%
     dplyr::pull(county)
 county <- counties[1]
 
-orange <- get_acs(
-  state = "CA",
-  county = "Orange",
+demo <- get_acs(
+  state = selected_state,
+  county = county,
   geography = "tract",
   variables = "B19013_001",
   geometry = TRUE,
-  year = 2020
-)
+  year = 2021
+) |> st_transform(crs= 4326)
+
+ggplot(demo, aes(fill = estimate, color = estimate)) +
+  geom_sf() +
+  coord_sf(crs = 26914) +
+  scale_fill_viridis(option = "magma") +
+  scale_color_viridis(option = "magma")
+
+leaflet() |>
+  addTiles() |>
+  addPolygons(demo, color = "#444444", weight = 1, smoothFactor = 0.5,
+            opacity = 1.0, fillOpacity = 0.5)
+plot(demo)
